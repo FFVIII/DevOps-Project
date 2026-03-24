29107f56a8a263a6cc17e7504e5696a2d487f765
fangvsyu <@fangvsyugmail.com>
2026-03-24 11:47:28 -0500

    testing dummy data to run in FastAPI

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get("/")
+#dummy data
+posts: list[dict] = [
+    {
+        "id": 1,
+        "author": "Dennis Fang",
+        "title": "FastAPI is Awesome",
+        "content": "This is a good framework.",
+        "date_posted": "March 24, 2026",
+    },
+    {
+        "id": 2,
+        "author": "Dennis Dennis",
+        "title": "Python is Great for Web Development",
+        "content": "Python is a great language for web development.",
+        "date_posted": "March 24, 2026",
+    },
+]
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+
+#reutrn HTML instead of JSON
+@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
 def home():
-    return {"message":"Hello World!"}
+    return f"<h1>{posts[1]['title']}</h1>"
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
