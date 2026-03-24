5c0599db9481dfc3115ab14266852708316d2347
fangvsyu <@fangvsyugmail.com>
2026-03-24 12:25:47 -0500

    install Jinja2, add templates file(home.html and layout.html) and update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+#import templates
+templates = Jinja2Templates(directory="templates")
 
 #dummy data
 posts: list[dict] = [
@@ -21,12 +24,14 @@ posts: list[dict] = [
     },
 ]
 
-@app.get("/", response_class=HTMLResponse, include_in_schema=False)
-
-#reutrn HTML instead of JSON
-@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{posts[1]['title']}</h1>"
+@app.get("/", include_in_schema=False)
+@app.get("/posts", include_in_schema=False)
+def home(request: Request):                                                
+    return templates.TemplateResponse(
+        request,
+        "home.html",
+        {"posts": posts, "title": "Home"},
+    )
 
 @app.get("/api/posts")
 def get_posts():
